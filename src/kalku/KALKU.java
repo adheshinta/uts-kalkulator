000d3af29b9d72c8d5fe3489e4fed81f17badd60
ADHESHINTA <ADHESHINTA@ADHES>
2016-11-17 10:32:26 -0800

    11140344_UTS Aplikasi Bisnis 2

---
+++ src/kalku/KALKU.java
@@ -0,0 +1,12 @@
+
+package kalku;
+
+import utskalkulator.frmKalkulator;
+
+public class KALKU {
+
+    public static void main(String[] args) {
+        new frmKalkulator(null, true).setVisible(true);
+    }
+    
+}
